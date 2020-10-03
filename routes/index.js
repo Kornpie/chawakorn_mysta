c1612c08c73882aa0819e5d3adc6b12f4fa30b7d
korn <korn@gmail.com>
2020-10-03 19:15:15 +0700

    555

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,5 @@ router.get('/',(req,res)=>{
 });
 
 
-router.get('/about',(req,res)=>{
-    res.render('about',{myData});
-});
-
-router.get('/education',(req,res)=>{
-    res.render('education',{myData});
-});
-
-router.get('/workexperience',(req,res)=>{
-    res.render('workexperience',{myData});
-});
-
-router.get('/skill',(req,res)=>{
-    res.render('skill',{myData});
-});
 
 module.exports = router;
